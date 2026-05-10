4530419cbea34b6ed79a9afda03070220941a6c6
dir310 <servisoftwaretencnologia@gmail.com>
2026-05-10 15:36:12 -0500

    fix: bump service worker cache to v2 to force refresh

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zippy-restaurado-v1';
+const CACHE_NAME = 'zippy-restaurado-v2';
 
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (e) => {
